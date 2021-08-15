ad0fbed362177ccd9765333bdbd4059840ff784a
pushpa287 <pushpabalusu97@gmail.com>
2021-08-15 15:03:29 +0530

    first commit'

---
+++ src/test/java/com/programming/techie/ConfigServer/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programming.techie.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
